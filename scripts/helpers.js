9e4b07312f167af0bdd8745a8db2ce02f6b524b7
Ilya Ilyankou <ilya.ilyankou@trincoll.edu>
2017-08-04 19:39:18 -0400

    Add table

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -16,3 +16,28 @@ function refreshWhenBack() {
 function daysBetween(start, end) {
   return Math.floor((end - start) / (1000 * 60 * 60 * 24)) + 1;
 }
+
+/**
+ * Returns a date in format MM-DD-YYYY
+ */
+function niceDate(d) {
+  return (d.getMonth() + 1) + '-' + d.getDate() + '-' + d.getFullYear();
+}
+
+/**
+ * Retrieves GET parameters from the URL
+ */
+function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
+  }
+};
